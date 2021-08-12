8cb8a2f0f556877afdb1794b9c318fc2c54a9145
Peter Linders <plinders@gmail.com>
2021-08-12 09:29:20 +0200

    automate depency installation

--- install.R
+++ install.R
@@ -1,20 +1,61 @@
 # install basic libraries
-# TODO: use R profile to set mirrors
-# install.packages("shiny", repos = "http://cran.us.r-project.org")
-# install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
-# install.packages("shinyBS", repos = "http://cran.us.r-project.org")
-# install.packages("readr", repos = "http://cran.us.r-project.org")
-# install.packages("rsvg", repos = "http://cran.us.r-project.org")
-# install.packages("shinyWidgets", repos = "http://cran.us.r-project.org")
-# install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
+
+if (!require(shiny)) {
+    install.packages("shiny")
+}
+
+if (!require(shinydashboard)) {
+    install.packages("shinydashboard")
+}
+
+if (!require(shinyBS)) {
+    install.packages("shinyBS")
+}
+
+if (!require(readr)) {
+    install.packages("readr")
+}
+
+if (!require(rsvg)) {
+    install.packages("rsvg")
+}
+
+if (!require(shinyWidgets)) {
+    install.packages("shinyWidgets")
+}
+
+if (!require(RColorBrewer)) {
+    install.packages("RColorBrewer")
+}
+
+if (!require(tidyverse)) {
+    install.packages("tidyverse")
+}
 
 # # install Manning-related libraries
-# install.packages("svgPanZoom", repos = "http://cran.us.r-project.org")
+
+if (!require(svgPanZoom)) {
+    install.packages("svgPanZoom")
+}
 
 # # install ui-related libraries
-# install.packages("colourpicker", repos = "http://cran.us.r-project.org")
-# install.packages("DT", repos = "http://cran.us.r-project.org")
+
+if (!require(colourpicker)) {
+    install.packages("colourpicker")
+}
+
+if (!require(DT)) {
+    install.packages("DT")
+}
+
 
 # # install other network libraries
-# install.packages("data.tree", repos = "http://cran.us.r-project.org")
-install.packages("jsonlite", repos = "http://cran.us.r-project.org")
+
+if (!require(data.tree)) {
+    install.packages("data.tree")
+
+}
+
+if (!require(jsonlite)) {
+    install.packages("jsonlite")
+}
